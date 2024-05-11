9cbd4c8811bfee940538f9a2d4960620f05f03fb
Cudiph <dwiaceromo@gmail.com>
2024-05-11 12:44:52 +0700

    chore: reconfigure dev environment

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,22 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [svelte()],
-  build: {
-    emptyOutDir: false,
-    rollupOptions: {
-      input: ['src/popup/index.html', 'src/options/index.html', 'src/service-worker.js'],
-      output: [
-        { dir: 'dist/chromium', entryFileNames: '[name].js' },
-        { dir: 'dist/firefox', entryFileNames: '[name].js' },
-      ],
+export default defineConfig((command, mode, isSsrBuild, isPreview) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+    plugins: [svelte()],
+    build: {
+      watch: env.NODE_ENV === "dev",
+      sourcemap: env.NODE_ENV === "dev",
+      emptyOutDir: env.NODE_ENV === "production",
+      rollupOptions: {
+        input: ['src/popup/index.html', 'src/options/index.html', 'src/service-worker.js'],
+        output: [
+          { dir: 'dist/chromium', entryFileNames: '[name].js' },
+          { dir: 'dist/firefox', entryFileNames: '[name].js' },
+        ],
+      },
     },
-  },
+  }
 });
